9fa8d895cfb62395f669f9dd89d0ed57955ebe11
Camilo Gomez <camigomezbernal@gmail.com>
2026-03-01 20:02:17 -0500

    feat: :sparkles: Commit inicial
    se agregan los archivos de estuido principales

---
+++ Ejemplos/variables.php
@@ -0,0 +1,81 @@
+<?php 
+$course = "Curso profesional de PHP y Laravel"; // variable
+$price = 30.00; //precio
+$fecha_publicacion = "13 de julio de 2025"; //Fecha
+$mensaje = "Este curso no solo te enseñará PHP… te enseñará a pensar como backend, dominar Laravel, y construir aplicaciones sólidas, seguras y escalables. Ideal para quienes quieren ir más allá de copiar y pegar código."; //mensaje
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
+    <title>
+        <?= $course?>
+    </title>
+    <style>
+    body {
+        font-family: 'Courier New', Courier, monospace;
+        background: linear-gradient(to right, #4facfe, #00f2fe);
+        /*fondo gradiente */
+        display: flex;
+        flex-direction: column;
+        margin: 0;
+        padding: 20px;
+        margin-top: 100px;
+    }
+
+    h1 {
+        text-align: center;
+        color: #333;
+        font: 2rem bold;
+    }
+
+    p {
+        margin-bottom: 10px;
+        text-align: center;
+    }
+
+    .card {
+        background: white;
+        padding: 30px;
+        border-radius: 10px;
+        max-width: 600px;
+        margin: auto;
+        border-radius: 40px;
+        box-shadow: 4px 8px 16px rgba(0, 0, 0, 0.8);
+    }
+
+    .fecha {
+        color: red;
+        font: 2rem bold;
+        margin-top: 10px;
+    }
+
+    .precio {
+        color: green;
+        font: 2rem bold;
+        margin-top: 10px;
+    }
+    </style>
+</head>
+
+<body>
+    <div class="card">
+        <h1>Bienvenido al <?= $course?></h1>
+        <!-- INFORMACIÓN -->
+        <p><?= $mensaje?></p>
+
+        <!-- DATOS -->
+        <section>
+            <div class="datos">
+                <p class="fecha">Este curso se ha publicado en: <?= $fecha_publicacion?></p>
+                <p class="precio"> Tiene un valor de: <?= $price ?> dolares</p>
+            </div>
+        </section>
+    </div>
+</body>
+
+</html>
---
+++ course.php
@@ -0,0 +1,150 @@
+<?php 
+
+//INFORMACIÓN PRINCIPAL
+class Course { //CLASE
+
+    //AHORA, se especifica que es una propiedad publica y su tipo de valor
+    public function __construct(//CONSTRUCTOR
+        //PROPIEDADES
+        protected string $title,
+        protected string $subtitle,
+        protected string $description,
+        protected array $lenguajes,
+        protected array $cursos,
+        protected array $lecciones,
+        protected ?Autor $autor = null,
+
+    ) {
+        //
+    }
+
+     public function _gettitle($title) {
+         if (property_exists($this, $title)) {
+             return $this->$title;
+         }
+         return null;
+     }
+
+     public function setAutor(Autor $autor): void
+    {
+        $this->autor = $autor;
+    }
+
+
+    public function __toString()
+    {
+        $nivel_curso = "Intermedio";
+        $mensaje_nivel = "Ideal si ya sabes lo básico 🚀";
+        $fecha_publicacion = date('Y-m-d');
+        $price = 49;
+        $status = "ACTIVO";
+        $estilo = $status === "ACTIVO" 
+            ? "estado estado-activo" 
+            : "estado estado-archivado";
+
+        $html = "<div class='card'>";
+
+        // TITULOS
+        $html .= "<h1>Bienvenido al {$this->title}</h1>";
+        $html .= "<h2>{$this->subtitle}</h2>";
+        $html .= "<p class='descripcion'>{$this->description}</p>";
+
+        // NUEVOS
+        $html .= "<section class='nuevos'>";
+
+        // Lenguajes
+        $html .= "<div class='etiq'>";
+        $html .= "<p>Etiquetas de Lenguajes</p>";
+        $html .= "<ul class='list'>";
+        foreach ($this->lenguajes as $lenguaje) {
+            $html .= "<li>$lenguaje</li>";
+        }
+        $html .= "</ul>";
+        $html .= "</div>";
+
+        // Cursos
+        $html .= "<div class='cursos'>";
+        $html .= "<p>Cursos Relacionados</p>";
+        $html .= "<ul class='list'>";
+        foreach ($this->cursos as $curso) {
+            $html .= "<li>$curso</li>";
+        }
+        $html .= "</ul>";
+        $html .= "</div>";
+
+        $html .= "</section>";
+
+        // Nivel
+        $html .= "<p class='nivel'>Este curso es de nivel: $nivel_curso, $mensaje_nivel</p>";
+
+        // Lecciones
+        $html .= "<section>";
+        $html .= "<p class='leccionesT'>Lecciones</p>";
+        $html .= "<div class='lecciones'>";
+        $html .= "<ul class='list'>";
+        foreach ($this->lecciones as $leccion) {
+            $html .= "<li>$leccion</li>";
+        }
+        $html .= "</ul>";
+        $html .= "</div>";
+        $html .= "</section>";
+
+        // Datos
+        $html .= "<section>";
+        $html .= "<div class='datos'>";
+        $html .= "<p class='fecha'>Publicado en: $fecha_publicacion</p>";
+        $html .= "<p class='precio'>Valor: $price dolares</p>";
+        $html .= "</div>";
+        $html .= "</section>";
+
+        // Estado
+        $html .= "<p class='$estilo'>ESTE CURSO ESTÁ $status</p>";
+
+        // Autor (si existe)
+        if ($this->autor !== null) {
+            $html .= $this->autor;
+        }
+
+        $html .= "</div>";
+
+        return $html;
+    }
+
+
+    public function addcursos(string $cursos): void {
+        if(in_array($cursos, $this->cursos)) {
+        return;
+        }
+
+        if(empty($cursos)) {
+        return;
+        }
+        if(count($this->cursos) >= 10) {
+        return;
+        }
+
+        $this->cursos[] = $cursos;
+    }
+}
+
+class Autor { //CLASE
+
+   //se especifica que es una propiedad publica y su tipo de valor
+    public function __construct(//CONSTRUCTOR
+            //PROPIEDADES
+            protected string $name,
+            protected DateTime $fecha,
+            protected string $ubicacion,
+            protected Course $course
+        ) {
+
+    }
+
+    public function __toString() {
+         $html = "<p class='autor'>{$this->name}</p>";
+         $html .= "<p class='fechCre'>{$this->fecha->format('Y-m-d')}</p>"; 
+         $html .= "<p class='lugar'>{$this->ubicacion}</p>"; 
+         $html .= "<h3 class='descripcion'>Curso: {$this->course->_gettitle('title')}</h3>";
+         return $html; 
+    }
+}

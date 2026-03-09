36f27d316921f06e0f9360c5b8808b04e2b6a966
Camilo Gomez <camigomezbernal@gmail.com>
2026-03-08 20:22:27 -0500

    Revert "Merge branch 'prueba_bot' into bot_final"
    This reverts commit f84bc879560472b11eb4721398f38b7ecf657d41, reversing
    changes made to 73bb6e4f07ecf5e83a04a1e8c59eacf7e8bdd242.

---
+++ src/courseType.php
@@ -0,0 +1,18 @@
+<?php  
+
+namespace Estudio_PHP;
+
+    enum CourseType: string {
+        case FREE = 'Gratis';
+        case PAID = 'Pago';
+        case VIP = 'VIP';
+
+        public function label(): string
+        {
+            return match($this) {
+                self::FREE => 'Curso gratuito',
+                self::PAID => 'Curso de pago',
+                self::VIP => 'Curso Premium',
+            };
+        }
+    }

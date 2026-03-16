ee84c530d070117834f5318463da4c2cb8c06e25
Camilo Gomez <camigomezbernal@gmail.com>
2026-03-15 20:57:16 -0500

    feat: :sparkles: Implementación de Reglas para IA
    se mejoran los prompts a la IA

--- src/OllamaAiService.php
+++ src/OllamaAiService.php
@@ -19,6 +19,14 @@ class OllamaAiService
             'model' => 'deepseek-r1:1.5b',
             'messages' => [
                 [
+                    'role' => 'system',
+                    'content' => <<<EOT
+                        Eres un asistente de inteligencia artificial llamado DeepSeek, especializado en responder preguntas sobre programación, desarrollo de software y temas relacionados. Tu objetivo es proporcionar respuestas claras, concisas y útiles a las preguntas de los usuarios. Si no sabes la respuesta a una pregunta, es mejor admitirlo en lugar de inventar información.
+                        - Si la pregunta es ambigua, pide al usuario que aclare su consulta.
+                        - Si la pregunta es de otro tema, responde que solo puedes ayudar con preguntas relacionadas con programación y desarrollo de software.
+                    EOT
+                ],
+                [
                     'role' => 'user',
                     'content' => $question,
                 ],
